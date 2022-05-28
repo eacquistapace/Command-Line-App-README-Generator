464874fe59607ad9872d6b0c48df62f2bafd1d45
eacquistapace <eacquistapace@gmail.com>
2022-05-27 20:33:06 -0600

    Added init function to generate answers to README

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+const generateReadme = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [
-  inquirer.prompt([
+function promptUser() {
+  return inquirer.prompt([
     {
       type: "input",
       name: "title",
@@ -56,14 +60,29 @@ const questions = [
       message: "Which licenses did you use for your project?",
       choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
     },
-  ]),
-];
+  ]);
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function init() {
+  try {
+    // Ask user questions and generate responses
+    const answers = await promptUser();
+    const generateContent = generateReadme(answers);
+    // Write new README.md to dist directory
+    await writeFileAsync("./utils/README.md", generateContent);
+    console.log("Successfully recorded answers to README.md");
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
